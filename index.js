2bc99efaf22bcf3df8775d551552132bc0a33bf3
Giuseppe Marinelli <marinelli.giuseppe01@gmail.com>
2023-05-11 14:58:23 +0200

    feat

--- index.js
+++ index.js
@@ -14,11 +14,5 @@ fs.copyFileSync(path.resolve(__dirname, "../templates/helpers/HttpContext.ts"), 
 // Sposta il file AuditMigration.ts nella cartella "database/migrations"
 fs.copyFileSync(path.resolve(__dirname, "../templates/migrations/AuditMigration.ts"), path.resolve(__dirname, "../../database/migrations/{timestamp}_audit_migration.js"));
 
-// // Aggiungi il middleware globalmente nel file "start/kernel.ts"
-// const kernelPath = path.resolve(__dirname, "../../start/kernel.ts");
-// let kernelContent = fs.readFileSync(kernelPath, "utf-8");
-// kernelContent = kernelContent.replace("use('Adonis/Middleware/Auth')", "use('Adonis/Middleware/Auth')\n  .use('App/Middleware/AuditMiddleware')");
-// fs.writeFileSync(kernelPath, kernelContent, "utf-8");
-
 // Esegui il comando per effettuare le migrazioni
 execSync("node ace migration:run");
