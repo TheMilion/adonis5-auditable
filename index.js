db2aa843e5d4e631dd0415d0b3a63f918b5df21f
Giuseppe Marinelli <marinelli.giuseppe01@gmail.com>
2023-05-12 12:45:58 +0200

    feat

--- index.js
+++ index.js
@@ -5,22 +5,13 @@ const { execSync } = require("child_process");
 var mkdirp = require("mkdirp");
 
 let modelsFolder = path.resolve(__dirname, "../../app/Models");
-let middlewareFolder = path.resolve(__dirname, "../../app/Middleware");
-let helpersFolder = path.resolve(__dirname, "../../app/Helpers");
 let migrationFolder = path.resolve(__dirname, "../../database/migrations");
 
 mkdirp.sync(modelsFolder);
-mkdirp.sync(middlewareFolder);
-mkdirp.sync(helpersFolder);
 mkdirp.sync(migrationFolder);
+
 // Sposta il file AuditModel.ts nella cartella "app/Models"
 fs.copyFileSync(path.resolve(__dirname, "templates/models/AuditModel.ts"), path.resolve(modelsFolder, "AuditModel.ts"));
-
-// Sposta il file AuditMiddleware.ts nella cartella "app/Middleware"
-fs.copyFileSync(path.resolve(__dirname, "templates/middleware/AuditMiddleware.ts"), path.resolve(middlewareFolder, "AuditMiddleware.ts"));
-
-// Sposta il file HttpContext.ts nella cartella "app/Helpers/"
-fs.copyFileSync(path.resolve(__dirname, "templates/helpers/HttpContext.ts"), path.resolve(helpersFolder, "HttpContext.ts"));
 
 const timestamp = new Date().getTime();
 const migrationFileName = `${timestamp}_audit_migration.ts`;
--- templates/middleware/AuditMiddleware.ts
+++
@@ -1,9 +0,0 @@
-import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import HttpContext from 'App/Helpers/HttpContext'
-
-export default class AuditMiddleware {
-    public async handle(ctx: HttpContextContract, next: () => Promise<void>) {
-        HttpContext.setContext(ctx)
-        await next()
-    }
-}
